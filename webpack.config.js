72c2aa3b3ff8f382fe48678b833f4609218059f4
Alex-Rudoy <rudoy4ik@gmail.com>
2020-06-09 18:20:13 +0300

    a lot of stuff basically

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const postcssPlugins = [
+    require("postcss-import"),
+    require("postcss-mixins"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
     require("autoprefixer")
@@ -11,15 +13,23 @@ module.exports = {
         filename: "bundled.js",
         path: path.resolve(__dirname, "app")
     },
+    devServer: {
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        before: function(app, server){
+            server._watch("./app/**/*.html")
+        },
+        host: "0.0.0.0"
+    },
     mode: "development",
-    watch: true,
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: [
                     'style-loader',
-                    'css-loader',
+                    'css-loader?url=false',
                     {
                         loader: 'postcss-loader',
                         options: {
