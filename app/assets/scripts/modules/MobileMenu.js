12eba28f6915ec57784dc86bc321b5585343b9a5
Alex-Rudoy <rudoy4ik@gmail.com>
2020-06-11 22:44:23 +0300

    reveal on scroll + prettified all files

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,20 +1,25 @@
 class MobileMenu {
-    constructor (){
-        this.menuIcon = document.querySelector(".site-header__menu-icon");
-        this.menuContent = document.querySelector(".site-header__menu-content");
-        this.siteHeader = document.querySelector(".site-header");
-        this.events();
-    }
+  constructor() {
+    this.menuIcon = document.querySelector(".site-header__menu-icon");
+    this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
+    this.events();
+  }
 
-    events(){
-        this.menuIcon.addEventListener("click", () => this.toggleMenu());
-    }
+  events() {
+    this.menuIcon.addEventListener("click", () => this.toggleMenu());
+    document.addEventListener("DOMContentLoaded", this.preventFlash.bind(this));
+  }
 
-    toggleMenu(){
-        this.menuContent.classList.toggle("site-header__menu-content--visible");
-        this.siteHeader.classList.toggle("site-header--visible");
-        this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
-    }
+  preventFlash() {
+    this.menuContent.classList.add("site-header__menu-content--loaded");
+  }
+
+  toggleMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--visible");
+    this.siteHeader.classList.toggle("site-header--visible");
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
+  }
 }
 
 export default MobileMenu;
