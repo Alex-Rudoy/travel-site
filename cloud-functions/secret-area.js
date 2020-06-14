639f5e261ef99087eae1fa0335d1d2c3d6ff355f
Alex-Rudoy <rudoy4ik@gmail.com>
2020-06-14 06:26:21 +0300

    aaaaaand another test 401 fix #14

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -26,11 +26,19 @@ exports.handler = function (event, context, callback) {
   if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: secretContent,
     });
   } else {
     callback(null, {
       statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
     });
   }
 };
